9b4b739814b8c3008616f6015edff9c790ee1d09
adityasunny1189 <adityapathak1189@gmail.com>
2020-01-09 21:48:19 +0530

    GCD of two numbers

---
+++ GCD ALGORITHMS/euclids_algorithm.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Function returning gcd of two entered numbers
+int gcd(int big, int small) {
+  int rem = 1;
+  while(1) {
+    rem = big % small;
+    if(rem == 0)
+      return small;
+    else {
+      big = small;
+      small = rem;
+    }
+  }
+}
+
+int main() {
+  int big, small;
+  cout << "Enter first number: ";
+  cin >> big;
+  cout << "Enter second number: ";
+  cin >> small;
+  if(big < small) {
+    int temp = small;
+    small = big;
+    big = temp;
+  }
+  cout << "GCD of entered numbers is: " << gcd(big, small) << endl;
+  return 0;
+}
