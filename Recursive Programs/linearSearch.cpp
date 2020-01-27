c35a72046460e3e896f8a002666b804d8583ff72
adityasunny1189 <adityapathak1189@gmail.com>
2020-01-27 23:15:51 +0530

    recursion

---
+++ Recursive Programs/linearSearch.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+void linearSearch(vector<int> v, int size, int ele) {
+  static int found = 0;
+  if(v[size - 1] == ele) {
+    found = 1;
+    cout << "Element found at: " << size - 1 << endl;
+  }
+  else
+    linearSearch(v, --size, ele);
+  if(found == 0)
+    cout << "Element not present" << endl;
+}
+int main() {
+  vector<int> v;
+  int size;
+  cout << "Enter size: ";
+  cin >> size;{
+    int num;
+    cin >> num;
+    v.push_back(num);
+  }
+  cout << "Array content are: ";
+  for(int i = 0; i < size; i++)
+    cout << v[i] << " ";
+  cout << endl;
+  int ele;
+  cout << "Enter element to search: ";
+  cin >> ele;
+  linearSearch(v, size, ele);
+  return 0;
+}
