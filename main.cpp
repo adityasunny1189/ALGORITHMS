408e04db4308ccf6b75a0a45c5bba953dd9b413d
Aditya Pathak <46691394+adityasunny1189@users.noreply.github.com>
2020-02-10 16:38:09 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1,n2;
+    n1 = mid - left + 1;
+    n2 = right - mid;
+    int Larr[n1], Rarr[n2];
+    for(int i = 0; i < n1; i++)
+        Larr[i] = arr[left + i];
+    for(int j = 0; j < n2; j++)
+        Rarr[j] = arr[mid + 1 + j];
+    int i = 0, j = 0, k = left;
+    while(i < n1 && j < n2)
+    {
+        if(Larr[i] <= Rarr[j])
+        {
+            arr[k] = Larr[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = Rarr[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < n1)
+    {
+        arr[k] = Larr[i];
+        i++;
+        k++;
+    }
+    while(j < n2)
+    {
+        arr[k] = Rarr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(int arr[], int left, int right)
+{
+    if(right > left)
+    {
+        int mid = (right + left) / 2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Entered unshorted array is: " << endl;
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    mergesort(arr, 0, n - 1);
+    cout << "Entered shorted array is: " << endl;
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
